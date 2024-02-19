86067677a30e26f4231410fc2ccd45afb064a0f1
Fxplorer <154011238+Fxplorer@users.noreply.github.com>
2024-02-19 17:48:24 -0500

    Simple Avalonia Hello World
    Simplist Avalonia implementation I could find.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,9 @@
+﻿
+
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Themes.Fluent;
+
+Application app = Application.Current ?? AppBuilder.Configure<Application>().UsePlatformDetect().SetupWithoutStarting().Instance;
+app.Styles.Add(new FluentTheme());
+app.Run(new Window() { Title = "Avalonia Basic Example", Content = "Hello Avalonia!" });
